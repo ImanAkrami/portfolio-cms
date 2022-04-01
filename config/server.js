8226694ae6ec0f699229e40c4b52e38c589aaf86
Iman Akrami <iman.akrami@gmail.com>
2022-04-01 22:16:29 +0430

    fix

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("Atim123456"),
+    keys: env.array("APP_KEYS"),
   },
 });
